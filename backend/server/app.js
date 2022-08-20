021f8d1c921168a5e890da5a2af1b85a6efd909f
janvieric <janvieric.fullstack@gmail.com>
2022-08-20 19:45:11 +0200

    Ajout d'un garde d'authentification

--- backend/server/app.js
+++ backend/server/app.js
@@ -3,22 +3,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const http_errors_1 = __importDefault(require("http-errors"));
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
-const body_parser_1 = __importDefault(require("body-parser"));
 const morgan_1 = __importDefault(require("morgan"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const index_route_1 = __importDefault(require("./routes/index.route"));
 require("dotenv").config({ path: "./.env" });
-const index_route_2 = __importDefault(require("./routes/index.route"));
-const user_route_1 = __importDefault(require("./routes/user.route"));
 const app = (0, express_1.default)();
 app.use((0, morgan_1.default)("dev"));
 app.use((0, cookie_parser_1.default)());
-app.use(body_parser_1.default.json());
-app.use(body_parser_1.default.urlencoded({ extended: false }));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use(express_1.default.static(path_1.default.resolve("../frontend/client")));
@@ -29,17 +23,6 @@ mongoose_1.default.connect(process.env.DATABASE_URL, function (error) {
     else {
         console.log("Connexion à la base de données établie.");
     }
-});
-app.use("/", index_route_2.default);
-app.use("/user", user_route_1.default);
-app.use((next) => {
-    next((0, http_errors_1.default)(404));
-});
-app.use((err, req, res) => {
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
-    res.status(err.status || 500);
-    res.render("error");
 });
 app.use(index_route_1.default);
 app.get("*", (_, res) => {
